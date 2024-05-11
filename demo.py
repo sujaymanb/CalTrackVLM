bb87483e4c8fe2621d623e162f63fbc27803a212
Sujay Bajracharya <sujaybajracharya@Sujays-MacBook-Air.local>
2024-05-11 09:26:37 -0500

    basic bunny demo

--- demo.py
+++ demo.py
@@ -1,25 +1,27 @@
 import gradio as gr
-from transformers import pipeline
+from bunny import Bunny
 
-model = pipeline("text-generation")
+# load VLM
+model = Bunny()
 
+def template(prompt):
+    """baseline template"""
+    sub_command = 'Estimate calories and macro nutrients based on what the food is and the estimated amount of food in the picture.'
+    text = f"A user asks an artificial intelligence diet assistant to help with their diet. The assistant gives accurate and concise estimates about the nutrition content would based on photos of their meals. {sub_command} USER: <image>\n{prompt} ASSISTANT:"
 
-def predict(prompt):
-    completion = model(prompt)[0]["generated_text"]
-    return completion
+    return text
 
 def test_multi(image, prompt):
-    return "prompt: " + prompt
+    """demo function calls model run function"""
+    text = template(prompt)
+    return model.run(text,image)
 
-#demo = gr.Interface(fn=predict, inputs="text", outputs="text")
-
+# Simple demo
 image = gr.Image(shape=(224, 224))
-
 demo = gr.Interface(
             fn=test_multi,
             inputs=[image,"text"],
             outputs="text"
         )
-
 
 demo.launch()
