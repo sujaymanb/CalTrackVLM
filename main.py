5c96e54c8ce5977492573773041782c04100f7fd
vijaymanbajracharya <bajracharyavijay@gmail.com>
2024-07-06 23:39:44 +0000

    removed HTML class response

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 from fastapi import FastAPI, File, UploadFile, Request
-from fastpi.responses import HTMLResponse, RedirectResponse
+from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
@@ -11,7 +11,7 @@ templates = Jinja2Templates(directory="templates")\
 
 images = []
 
-@app.get("/", response_class=HTMLResponse)
+@app.get("/")
 async def home(request: Request):
     return templates.TemplateResponse(
         "home.html", {"request": request, "videos": videos})
