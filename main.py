abfbc4ffc35a628e7ea0e5b566c300238898a172
vijaymanbajracharya <bajracharyavijay@gmail.com>
2024-07-07 01:14:26 +0000

    added bunny LM model

--- main.py
+++ main.py
@@ -3,18 +3,33 @@ from fastapi import FastAPI, File, UploadFile, Request
 from fastapi.responses import RedirectResponse, HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from bunny import Bunny
 
 app = FastAPI()
 os.makedirs("static", exist_ok=True)
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
-images = []
+responses = []
+
+# Model
+model = Bunny()
+
+def template(prompt):
+    sub_command = 'Estimate calories and macro nutrients based on what the food is and the estimated amount of food in the picture.'
+    text = f"A user asks an artificial intelligence diet assistant to help with their diet. The assistant gives accurate and concise estimates about the nutrietion content would based on photos of their meals. {sub_command} USER: <image>\n{prompt}
+ASSISTANT:"
+
+    return text
+
+def test_multi(image, prompt):
+    text = template(prompt)
+    return model.run(text, image)
 
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
     return templates.TemplateResponse(
-        "home.html", {"request": request, "images": images})
+        "home.html", {"request": request, "response": responses})
 
 @app.post("/uploadimage/")
 async def upload_image(image: UploadFile = File(...)):
@@ -22,6 +37,8 @@ async def upload_image(image: UploadFile = File(...)):
     with open(file_loc, "wb+") as file:
         file.write(image.file.read())
     
-    new_image = image.filename #process_image(image.filename)
-    images.append(new_image)
+    text = template("")
+    output = model.run(text, image.filename)
+    responses.append(output)
+    
     return RedirectResponse(url="/", status_code=303)
