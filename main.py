1192bbb2f1f45073c803c1536e567acee6797cfe
Sujay Bajracharya <sujaymanb@gmail.com>
2024-07-06 19:43:42 -0500

    update template formate, fix echo in main

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 import os
 from fastapi import FastAPI, File, UploadFile, Request
-from fastapi.responses import RedirectResponse
+from fastapi.responses import RedirectResponse, HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 os.makedirs("static", exist_ok=True)
 app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")\
+templates = Jinja2Templates(directory="templates")
 
 images = []
 
-@app.get("/")
+@app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
     return templates.TemplateResponse(
         "home.html", {"request": request, "images": images})
 
 @app.post("/uploadimage/")
 async def upload_image(image: UploadFile = File(...)):
-    new_image = image #process_image(image.filename)
+    new_image = image.filename #process_image(image.filename)
     images.append(new_image)
     return RedirectResponse(url="/", status_code=303)
