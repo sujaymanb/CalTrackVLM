4a948f5811486c8318e76c168aea3f67896d03f1
Sujay Bajracharya <sujaymanb@gmail.com>
2024-08-22 18:13:14 -0500

    update versions, set data path, use chromadb

--- rag.py
+++ rag.py
@@ -1,4 +1,4 @@
-''' requirements
+''' old requirements
 !pip install -q langchain
 !pip install -q torch
 !pip install -q transformers
@@ -7,37 +7,41 @@
 !pip install -q faiss-cpu
 '''
 
-from langchain.document_loaders import HuggingFaceDatasetLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.vectorstores import FAISS
-from transformers import AutoTokenizer, AutoModelForQuestionAnswering
-from transformers import AutoTokenizer, pipeline
-from langchain import HuggingFacePipeline
+#from langchain.document_loaders import HuggingFaceDatasetLoader
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
+#from transformers import AutoTokenizer, AutoModelForQuestionAnswering
+#from transformers import AutoTokenizer, pipeline
 from langchain.chains import RetrievalQA
+from langchain_community.document_loaders import TextLoader
+from langchain_chroma import Chroma
+#import glob
+import os
 
 
 class RAG:
     def __init__(self,
-            dataset_name,
-            column_name,
+            docpath="./data",
             embedding_model_path="sentence-transformers/all-MiniLM-l6-v2",
             qa_model_path="Intel/dynamic_tinybert",
             debug=True):
         self.debug = debug
 
         # init loader
-        self.loader = HuggingFaceDatasetLoader(
-                        dataset_name, 
-                        column_name)
+        # self.loader = HuggingFaceDatasetLoader(
+        #                 dataset_name, 
+        #                 column_name)
+
+        documents = []
+
+        for file in os.listdir(docpath):
+            if file.endswith('.txt'):
+                path = os.path.join(docpath, file)
+                documents.extend(TextLoader(path).load())
 
         # split into docs
-        data = loader.load()
-        self.text_splitter = RecursiveCharacterTextSplitter(
-                                chunk_size=1000, 
-                                chunk_overlap=150)
-
-        self.docs = text_splitter.split_documents(data)
+        text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=10)
+        documents = text_splitter.split_documents(documents)
 
         # load embeddings models
         model_kwargs = {'device':'cuda'}
@@ -50,25 +54,25 @@ class RAG:
 
         # vector database
         print("loading vector database...")
-        self.db = FAISS.from_documents(self.docs, self.embeddings)
+        self.db = Chroma.from_documents(documents, self.embeddings)
 
-        # load the QA model
-        tokenizer = AutoTokenizer.from_pretrained(qa_model_path, padding=True, truncation=True, max_length=512)
-        self.llm = pipeline(
-            "question-answering", 
-            model=qa_model_path, 
-            tokenizer=tokenizer,
-            return_tensors='pt'
-        )
+        # # load the QA model
+        # tokenizer = AutoTokenizer.from_pretrained(qa_model_path, padding=True, truncation=True, max_length=512)
+        # self.llm = pipeline(
+        #     "question-answering", 
+        #     model=qa_model_path, 
+        #     tokenizer=tokenizer,
+        #     return_tensors='pt'
+        # )
 
         # retrieval chain
         self.retriever = self.db.as_retriever(search_kwargs={"k": 4})
-        self.qa_chain = RetrievalQA.from_chain_type(llm=self.llm, chain_type="refine", retriever=self.retriever, return_source_documents=False)
+        #self.qa_chain = RetrievalQA.from_chain_type(llm=self.llm, chain_type="refine", retriever=self.retriever, return_source_documents=False)
 
         # debug logs
-        if self.debug:
-            print(data[:2])
-            print(docs[0])
+        # if self.debug:
+        #     print(data[:2])
+        #     print(docs[0])
 
 
     def get_embeddings(self, text):
@@ -83,6 +87,7 @@ class RAG:
 
         return result_docs[0].page_content
 
+
     def retrieve(self, prompt, k=1, search_type="mmr"):
         retriever = self.db.as_retriever(search_type=search_type)
         search_kwargs={"k": k}
@@ -90,35 +95,31 @@ class RAG:
 
         return docs
 
-    def run(self, prompt, return_all = False):
-        result = self.qa_chain.run({"query": prompt})
-        if return_all:
-            return result
+    # def run(self, prompt, return_all = False):
+    #     result = self.qa_chain.run({"query": prompt})
+    #     if return_all:
+    #         return result
 
-        return result["result"]
+    #     return result["result"]
 
 
 def main():
-    dataset_name = "databricks/databricks-dolly-15k"
-    page_content_column = "context"
-    
     # init rag
-    rag = RAG(dataset_name, page_content_column)
+    rag = RAG()
 
     # test embeddings
-    text = "This is a test."
+    text = "Grilled Cheese Nutrition"
     result = rag.get_embeddings(text)
 
     print(f"embeddings:\n{result[:3]}")
 
     # test vector db search
-    question = "What is alopecia?"
-    result = rag.db_search(question)
+    result = rag.db_search(text)
 
     print(f"search results:\n{result}")
 
-    # invoke chain
-    result = rag.run("Who is George Washington?")
-    print(result)
+    # # invoke chain
+    # result = rag.run("Who is George Washington?")
+    # print(result)
 
 main()
